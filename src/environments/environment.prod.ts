eb46733f8a8f06710aee6828df3998b990d5d2ca
XLR8-07 <joyashikur@gmail.com>
2020-06-16 11:28:10 +0600

    intial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC3bO3oG5gPF4_15cvC85WQu9Lvuv8wDEE",
+    authDomain: "rebook-3bf0f.firebaseapp.com",
+    databaseURL: "https://rebook-3bf0f.firebaseio.com",
+    projectId: "rebook-3bf0f",
+    storageBucket: "rebook-3bf0f.appspot.com",
+    messagingSenderId: "1614867652",
+    appId: "1:1614867652:web:ab6c583ce52ee088602d91",
+    measurementId: "G-L8LEM5C79H"
+  }
+};
