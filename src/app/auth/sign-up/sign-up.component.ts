333c715a980f8fc78af27213deb8dfffd3017f07
XLR8-07 <ashiqurrahman85@iut-dhaka.edu>
2020-10-14 23:56:53 +0600

    Sign Up form Implemented with registration

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/shared/auth.service';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,8 +13,14 @@ export class SignUpComponent implements OnInit {
 
   hide = true;
   selectedImage: any = null;
-  imgSrc : string = '/assets/img/image_placeholder.jpg';
+  imgSrc : string = '/assets/img/image_placeholder2.png';
   isSubmitted : boolean = false;
+  Taglist: string[] = [];
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
   signUpForm : FormGroup = new FormGroup({
     Name : new FormControl('',Validators.required),
@@ -20,9 +28,11 @@ export class SignUpComponent implements OnInit {
     Password : new FormControl('',Validators.required),
     Work : new FormControl('',Validators.required),
     PhoneNo : new FormControl('',Validators.required),
-    imageURL : new FormControl('',Validators.required)
-
-
+    imageURL : new FormControl('',Validators.required),
+    Address : new FormControl('',Validators.required),
+    Description : new FormControl('',Validators.required),
+    Proficiency: new FormControl(this.Taglist),
+    Job : new FormControl('',Validators.required)
   })
 
   constructor(public Authservice: AuthService) { }
@@ -37,6 +47,33 @@ export class SignUpComponent implements OnInit {
     this.Authservice.signUp(email,password);
   }
 
+  addTag(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.Taglist.push(value.trim());
+    }
+
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(Tag: string): void {
+    const index = this.Taglist.indexOf(Tag);
+
+    if (index >= 0) {
+      this.Taglist.splice(index, 1);
+    }
+  }
+
+  search(text:any){
+    for (let l of text){
+      console.log(l)
+    }
+  }
+
   
   showPreview(event : any){
     if(event.target.files && event.target.files[0]){
@@ -46,7 +83,7 @@ export class SignUpComponent implements OnInit {
       this.selectedImage = event.target.files[0];
     }
     else{
-      this.imgSrc = '/assets/img/image_placeholder.jpg';
+      this.imgSrc = '/assets/img/image_placeholder2.png';
       this.selectedImage = null;
     }
   }
