75a4028bb5a4b74bad47d1dd705a7c8e1808e510
Abedeen <iftekharul25abedeen@gmail.com>
2020-10-24 20:27:05 +0600

    Rating Calculator added

--- src/app/dash-board/dash-board.component.ts
+++ src/app/dash-board/dash-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 import { AuthService } from '../shared/auth.service';
 
 @Component({
@@ -10,9 +11,23 @@ export class DashBoardComponent implements OnInit {
 
   listOfDom: string[] = ['job', 'Tour', 'Drawing', 'Fishing']
 
+  rating: number[] = [5, 3, 2, 2, 10]
+
   constructor(public authService : AuthService) { }
 
   ngOnInit(): void {
+    this.getRating()
   }
+
+  getRating(){
+    var count = 0;
+    var val = 0;
+    for (let i = 0; i < this.rating.length; i++) {
+      count += this.rating[i]
+      val += (i+1) * this.rating[i]
+    }
+    return (val/count).toFixed(2);
+  }
+
 
 }
